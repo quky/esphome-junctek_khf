564e666014651e34900c85185f9d79be1bff3142
quky <hidroela@gmail.com>
2025-12-27 09:08:23 -0600

    Update junctek_kgf.cpp
    edit 2

--- components/junctek_kgf/junctek_kgf.cpp
+++ components/junctek_kgf/junctek_kgf.cpp
@@ -269,7 +269,7 @@ void JuncTekKGF::loop()
 //todo not needed. uncomment if you do not receive any data, or dont use monitor, or monitor data refresh is turned of
 
   const unsigned long start_time = esphome::millis();
-//
+
   if (!this->last_settings_ || (*this->last_settings_ + this->update_settings_interval_) < start_time)
   {
     this->last_settings_ = start_time;
@@ -277,7 +277,7 @@ void JuncTekKGF::loop()
     sprintf(buffer, ":R51=%d,2,1,\r\n", this->address_);
     write_str(buffer);
   }
-//
+
   if (!this->last_stats_ || (*this->last_stats_ + this->update_stats_interval_) < start_time)
   {
     this->last_stats_ = start_time;
